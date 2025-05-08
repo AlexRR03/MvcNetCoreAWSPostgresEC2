e74f0527785270fb2c77fe71601aa47ade5349ac
Alex <alexciempo03@gmail.com>
2025-05-08 10:53:28 +0200

    Add project files.

---
+++ MvcNetCoreAWSPostgresEC2/Controllers/DepartamentosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreAWSPostgresEC2.Models;
+using MvcNetCoreAWSPostgresEC2.Repositories;
+
+namespace MvcNetCoreAWSPostgresEC2.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryHospital repo;
+        public DepartamentosController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Departamento> departamentos = await this.repo.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Departamento departamento = await this.repo.FindDepartamento(id);
+            return View(departamento);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Departamento departamento)
+        {
+           
+                await this.repo.AddDepartamento(departamento.Id, departamento.Nombre, departamento.Localidad);
+                return RedirectToAction("Index");
+            
+        }
+    }
+}
---
+++ MvcNetCoreAWSPostgresEC2/Repositories/RepositoryHospital.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCoreAWSPostgresEC2.Data;
+using MvcNetCoreAWSPostgresEC2.Models;
+
+namespace MvcNetCoreAWSPostgresEC2.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+        public RepositoryHospital( HospitalContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamento.ToListAsync();
+        }
+        public async Task<Departamento> FindDepartamento(int id)
+        {
+            return await this.context.Departamento.FirstOrDefaultAsync(x=>x.Id == id);
+        }
+        public async Task AddDepartamento( int id, string nombre, string localidad)
+        {
+            Departamento departamento = new Departamento();
+            departamento.Id = id;
+            departamento.Nombre = nombre;
+            departamento.Localidad = localidad;
+            this.context.Departamento.Add(departamento);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
